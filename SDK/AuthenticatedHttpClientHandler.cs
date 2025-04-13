f994d50106695c3500942e6907009d82c42c8bf5
beaniecazier <beanieroxiicazier@gmail.com>
2025-04-12 18:31:12 -0700

    Add JWT Authentication token passing

---
+++ SDK/AuthenticatedHttpClientHandler.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Gay.TCazier.Resume.SDK;
+
+public class AuthenticatedHttpClientHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthenticatedHttpClientHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var token = await _httpContextAccessor.HttpContext.GetTokenAsync("access_token");
+
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- SDK/SDKServiceCollectionExtensions.cs
+++ SDK/SDKServiceCollectionExtensions.cs
@@ -11,87 +11,94 @@ public static class SDKServiceCollectionExtensions
     {
         Log.Information("Now adding http client for SDK calls using base address {baseURL}", baseURL);
         services.AddHttpClient();
-
-        //.AddSingleton<AuthTokenProvider>()
-        //.AddRefitClient<IAddressEndpoints>(s => new RefitSettings
-        //{
-        //    AuthorizationHeaderValueGetter = async () => await s.GetRequiredService<AuthTokenProvider>()
-        //                                                          .GetTokenAsync()
-        //})
+        services.AddHttpContextAccessor();
+        services.AddTransient<AuthenticatedHttpClientHandler>();
 
         services.AddRefitClient<IAddressEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Address Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<ICountryCodeEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Country Code Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IContactEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Contact Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IOrganizationEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Organization Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IPersonEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Person Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IPhoneNumberEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Phone Number Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         
         services.AddRefitClient<ICertificateEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Certificate Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IEducationDegreeEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Degree Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IProjectEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Project Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IResumeEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Resume Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<ITechTagEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Tech Tag Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         services.AddRefitClient<IWorkExperienceEndpoints>()
             .ConfigureHttpClient(
             x => {
                 Log.Debug("Now assigning {baseURL} as the host url for Work Experience Model calls using the SDK", baseURL);
                 x.BaseAddress = new Uri(baseURL);
-            });
+            })
+            .AddHttpMessageHandler<AuthenticatedHttpClientHandler>();
         return services;
     }
 }
